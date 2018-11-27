1c0422440048aa3c348153a3c8c1e2455b1224e9
Mickey Vänskä <mvaenskae@gmail.com>
2018-11-27 02:43:42 +0100

    Plotting scripts extended with experiment definitions and completed single memtier parser.
    Signed-off-by: Mickey Vänskä <mvaenskae@gmail.com>

---
+++ chart_scripts/experiment_definitions.py
@@ -0,0 +1,166 @@
+class ExperimentDefinitions:
+    """
+    Helper class which holds constant definitions for the experimental parameters for each experiment
+    """
+
+    log_base="asl-logs-mickeyv"
+
+    @staticmethod
+    def subexpriment_21():
+        return {'experiment_id': 2,
+                'subexperiment_id': 1,
+                'worker_threads': [0],
+                'memtier_threads': [2],
+                'memtier_clients': [1, 2, 4, 8, 16, 32],
+                'hostnames': ['Client1', 'Client2', 'Client3'],
+                'request_types': ['GET', 'SET'],
+                'memcached_servers': ['Server1'],
+                'memtier_targets': ['Server1']}
+
+    @staticmethod
+    def subexpriment_22():
+        return {'experiment_id': 2,
+                'subexperiment_id': 2,
+                'worker_threads': [0],
+                'memtier_threads': [1],
+                'memtier_clients': [1, 2, 4, 8, 16, 32],
+                'hostnames': ['Client1'],
+                'request_types': ['GET', 'SET'],
+                'memcached_servers': ['Server1', 'Server2'],
+                'memtier_targets': ['Server1', 'Server2']}
+
+    @staticmethod
+    def subexpriment_31():
+        return {'experiment_id': 3,
+                'subexperiment_id': 1,
+                'worker_threads': [8, 16, 32, 64],
+                'memtier_threads': [2],
+                'memtier_clients': [1, 2, 4, 8, 16, 32],
+                'hostnames': ['Client1', 'Client2', 'Client3', 'Middleware1'],
+                'request_types': ['GET', 'SET'],
+                'memcached_servers': ['Middleware1'],
+                'memtier_targets': ['Server1']}
+
+    @staticmethod
+    def subexpriment_32():
+        return {'experiment_id': 3,
+                'subexperiment_id': 2,
+                'worker_threads': [8, 16, 32, 64],
+                'memtier_threads': [1],
+                'memtier_clients': [1, 2, 4, 8, 16, 32],
+                'hostnames': ['Client1', 'Client2', 'Client3', 'Middleware1', 'Middleware2'],
+                'request_types': ['GET', 'SET'],
+                'memcached_servers': ['Middleware1', 'Middleware2'],
+                'memtier_targets': ['Server1']}
+
+    @staticmethod
+    def subexpriment_40():
+        return {'experiment_id': 4,
+                'subexperiment_id': 0,
+                'worker_threads': [8, 16, 32, 64],
+                'memtier_threads': [1],
+                'memtier_clients': [1, 2, 4, 8, 16, 32],
+                'hostnames': ['Client1', 'Client2', 'Client3', 'Middleware1', 'Middleware2'],
+                'request_types': ['SET'],
+                'memcached_servers': ['Middleware1', 'Middleware2'],
+                'memtier_targets': ['Server1', 'Server2', 'Server3']}
+
+    @staticmethod
+    def subexpriment_51():
+        return {'experiment_id': 5,
+                'subexperiment_id': 1,
+                'worker_threads': [16],
+                'memtier_threads': [1],
+                'memtier_clients': [2],
+                'hostnames': ['Client1', 'Client2', 'Client3', 'Middleware1', 'Middleware2'],
+                'request_types': ['SHARDED_1', 'SHARDED_3', 'SHARDED_6', 'SHARDED_9'],
+                'memcached_servers': ['Middleware1', 'Middleware2'],
+                'memtier_targets': ['Server1', 'Server2', 'Server3']}
+
+    @staticmethod
+    def subexpriment_52():
+        return {'experiment_id': 5,
+                'subexperiment_id': 2,
+                'worker_threads': [16],
+                'memtier_threads': [1],
+                'memtier_clients': [2],
+                'hostnames': ['Client1', 'Client2', 'Client3', 'Middleware1', 'Middleware2'],
+                'request_types': ['MULTIGET_1', 'MULTIGET_3', 'MULTIGET_6', 'MULTIGET_9'],
+                'memcached_servers': ['Middleware1', 'Middleware2'],
+                'memtier_targets': ['Server1', 'Server2', 'Server3']}
+
+    @staticmethod
+    def subexpriment_60_1_1():
+        return {'experiment_id': 6,
+                'subexperiment_id': 0,
+                'worker_threads': [8, 32],
+                'memtier_threads': [2],
+                'memtier_clients': [32],
+                'hostnames': ['Client1', 'Client2', 'Client3', 'Middleware1'],
+                'request_types': ['GET', 'SET'],
+                'memcached_servers': ['Middleware1'],
+                'memtier_targets': ['Server1']}
+
+    @staticmethod
+    def subexpriment_60_1_3():
+        return {'experiment_id': 6,
+                'subexperiment_id': 0,
+                'worker_threads': [8, 32],
+                'memtier_threads': [2],
+                'memtier_clients': [32],
+                'hostnames': ['Client1', 'Client2', 'Client3', 'Middleware1'],
+                'request_types': ['GET', 'SET'],
+                'memcached_servers': ['Middleware1'],
+                'memtier_targets': ['Server1', 'Server2', 'Server3']}
+
+    @staticmethod
+    def subexpriment_60_2_1():
+        return {'experiment_id': 6,
+                'subexperiment_id': 0,
+                'worker_threads': [8, 32],
+                'memtier_threads': [1],
+                'memtier_clients': [32],
+                'hostnames': ['Client1', 'Client2', 'Client3', 'Middleware1', 'Middleware2'],
+                'request_types': ['GET', 'SET'],
+                'memcached_servers': ['Middleware1', 'Middleware2'],
+                'memtier_targets': ['Server1']}
+
+    @staticmethod
+    def subexpriment_60_2_3():
+        return {'experiment_id': 6,
+                'subexperiment_id': 0,
+                'worker_threads': [8, 32],
+                'memtier_threads': [1],
+                'memtier_clients': [32],
+                'hostnames': ['Client1', 'Client2', 'Client3', 'Middleware1', 'Middleware2'],
+                'request_types': ['GET', 'SET'],
+                'memcached_servers': ['Middleware1', 'Middleware2'],
+                'memtier_targets': ['Server1', 'Server2', 'Server3']}
+
+    @staticmethod
+    def all_experiments():
+        experiment2 = {'1': ExperimentDefinitions.subexpriment_21(), '2': ExperimentDefinitions.subexpriment_22()}
+        experiment3 = {'1': ExperimentDefinitions.subexpriment_31(), '2': ExperimentDefinitions.subexpriment_32()}
+        experiment4 = {'0': ExperimentDefinitions.subexpriment_40()}
+        experiment5 = {'1': ExperimentDefinitions.subexpriment_51(), '2': ExperimentDefinitions.subexpriment_52()}
+        experiment6 = {'1-1': ExperimentDefinitions.subexpriment_60_1_1(),
+                       '1-3': ExperimentDefinitions.subexpriment_60_1_3(),
+                       '2-1': ExperimentDefinitions.subexpriment_60_2_1(),
+                       '2-3': ExperimentDefinitions.subexpriment_60_2_3()}
+
+        experiments = {'2': experiment2, '3': experiment3, '4': experiment4, '5': experiment5, '6': experiment6}
+        return experiments
+
+
+    @staticmethod
+    def all_subexperiments():
+        experiments = {'21': ExperimentDefinitions.subexpriment_21(), '22': ExperimentDefinitions.subexpriment_22(),
+                       '31': ExperimentDefinitions.subexpriment_31(), '32': ExperimentDefinitions.subexpriment_32(),
+                       '40': ExperimentDefinitions.subexpriment_40(),
+                       '51': ExperimentDefinitions.subexpriment_51(), '52': ExperimentDefinitions.subexpriment_52(),
+                       '60_1-1': ExperimentDefinitions.subexpriment_60_1_1(),
+                       '60_1-3': ExperimentDefinitions.subexpriment_60_1_3(),
+                       '60_2-1': ExperimentDefinitions.subexpriment_60_2_1(),
+                       '60_2-3': ExperimentDefinitions.subexpriment_60_2_3()}
+
+        return experiments
--- chart_scripts/memtier_parser.py
+++ chart_scripts/memtier_parser.py
@@ -1,3 +1,5 @@
+import collections
+import math
 import re
 import sys
 
@@ -13,12 +15,12 @@ class MemtierParser:
         self.total_summary = ()
 
         # Histogram of query types based on percentages
-        self.get_histogram_percentage = [(0.0, 0.0)]
-        self.set_histogram_percentage = [(0.0, 0.0)]
+        self.get_histogram_percentage = {}
+        self.set_histogram_percentage = {}
 
         # Histogram of query types based on counts
-        self.set_histogram_counts = [(0.0, 0.0)]
-        self.get_histogram_counts = [(0.0, 0.0)]
+        self.set_histogram_counts = {}
+        self.get_histogram_counts = {}
 
     def parse_file(self, argc, argv):
         # Second argv is filename
@@ -58,8 +60,13 @@ class MemtierParser:
                     set_histogram_memtier.append(MemtierParser.parse_histogram_entry(line))
                     continue
 
-        self.set_histogram_percentage = MemtierParser.transform_from_cdf(set_histogram_memtier)
-        self.get_histogram_percentage = MemtierParser.transform_from_cdf(get_histogram_memtier)
+        memtier_set_histogram_percentage = dict(MemtierParser.transform_from_cdf(set_histogram_memtier))
+        memtier_get_histogram_percentage = dict(MemtierParser.transform_from_cdf(get_histogram_memtier))
+
+        self.get_histogram_percentage = MemtierParser.transform_to_our_buckets(memtier_get_histogram_percentage)
+
+        self.set_histogram_percentage = MemtierParser.transform_to_our_buckets(memtier_set_histogram_percentage)
+        self.get_histogram_percentage = MemtierParser.transform_to_our_buckets(memtier_get_histogram_percentage)
 
         self.set_histogram_counts = MemtierParser.percentages_to_counts(self.set_histogram_percentage, self.set_summary[0] * self.seconds)
         self.get_histogram_counts = MemtierParser.percentages_to_counts(self.get_histogram_percentage, self.get_summary[0] * self.seconds)
@@ -105,13 +112,44 @@ class MemtierParser:
         return [(elem1[0], elem1[1] - elem0[1]) for elem0, elem1 in zip(memtier_histogram, memtier_histogram[1:])]
 
     @staticmethod
+    def transform_to_our_buckets(percentages_memtier):
+        """
+        Returns for given memtier-based histogram a comparable histogram to our middleware's output.
+        :param percentages_memtier: Histogram as returned per memtier in format
+        :return: Histogram which has 100µs buckets between [0, 49.9] ms
+        """
+        result = {}
+
+        # Default initialize the result-dictionary to our needs
+        for i in range(500):
+            result[MemtierParser.get_rounded_double(i / 10)] = 0.0
+
+        sum_over_50 = 0
+        # Fill in the observed values to the dictionary
+        for item in percentages_memtier:
+            if item >= 50.0:
+                # Accumulate anything above 50 milliseconds
+                sum_over_50 += percentages_memtier.get(item)
+            else:
+                # Try to expand buckets and divide respective percentages evenly -> uniform distribution inside buckets
+                bucket_count = int(math.pow(10, math.ceil(math.log10(item)) - 1))
+                bucket_value = MemtierParser.safe_div(percentages_memtier.get(item), bucket_count)
+                for i in range(bucket_count):
+                    index = MemtierParser.get_rounded_double(item + i/10)
+                    result[index] = bucket_value
+
+        result['49.9'] += sum_over_50
+
+        return result
+
+    @staticmethod
     def percentages_to_counts(percentage_histogram, op_count):
         """
-        Function to transform an array of tuples with percentages to counts.
-        :param percentage_histogram: Array of tuples with percentages (non cumulative).
-        :return: Array of tuples (timestamp_range, counts)
+        Function to transform an dictionary of "timestamp: tuple" with percentages to counts.
+        :param percentage_histogram: Dictionary with percentages (non cumulative).
+        :return: Dictionary of "timestamp: counts"
         """
-        return [(timestamp, percentage/100 * op_count) for (timestamp, percentage) in percentage_histogram]
+        return {ts: percentage_histogram[ts]/100 * op_count for ts in percentage_histogram}
 
     @staticmethod
     def pretty_print_list(formatted_histogram):
@@ -120,7 +158,7 @@ class MemtierParser:
         :param formatted_histogram:
         :return: Array of tuples (timestamp, distribution) up to two decimal places.
         """
-        return ["(%.2f, %.2f)" % (timestamp, distribution) for (timestamp, distribution) in formatted_histogram]
+        return ["(%.1f, %.2f)" % (timestamp, distribution) for (timestamp, distribution) in formatted_histogram]
 
     @staticmethod
     def get_sane_double(s):
@@ -148,15 +186,40 @@ class MemtierParser:
         except ValueError:
             return 0
 
+    @staticmethod
+    def get_rounded_double(s):
+        """
+        Get a string representation of a rounded double up to 1 decimal place.
+        :param s: string to be interpreted as a float
+        :return: String representaton of `s` or 0.0 if unparseable.
+        """
+        try:
+            float(s)
+            return "{0:.1f}".format(float(s))
+        except ValueError:
+            return "0.0"
+
+    @staticmethod
+    def safe_div(x, y):
+        """
+        Helper method to safely divide two numbers
+        :param x: Dividend
+        :param y: Divisor
+        :return: Division of 0 if divisior is 0.
+        """
+        if y == 0:
+            return 0
+        return x / y
+
     def print_parsed(self):
         print(self.seconds)
         print(self.set_summary)
         print(self.get_summary)
         print(self.total_summary)
-        print(MemtierParser.pretty_print_list(self.set_histogram_percentage))
-        print(MemtierParser.pretty_print_list(self.get_histogram_percentage))
-        print(MemtierParser.pretty_print_list(self.set_histogram_counts))
-        print(MemtierParser.pretty_print_list(self.get_histogram_counts))
+        print(self.set_histogram_percentage)
+        print(self.get_histogram_percentage)
+        print(self.set_histogram_counts)
+        print(self.get_histogram_counts)
 
 
 if __name__ == '__main__':
